68616b164906f937834b324ca4f9a07372bcd531
Silver <zatmonhp@yandex.ru>
2026-04-25 12:42:34 +0300

    Hello-world: дерево решения из одного узла

--- src/main/java/com/github/CompPrehension/Main.java
+++ src/main/java/com/github/CompPrehension/Main.java
@@ -1,7 +1,35 @@
 package com.github.CompPrehension;
 
+import its.model.DomainSolvingModel;
+import its.reasoner.LearningSituation;
+import its.reasoner.nodes.DecisionTreeReasoner;
+import its.reasoner.nodes.DecisionTreeTrace;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            helloWorld();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private static void helloWorld() throws FileNotFoundException {
+        DomainSolvingModel model = new DomainSolvingModel("model", DomainSolvingModel.BuildMethod.LOQI);
+
+        LearningSituation situation = new LearningSituation(
+                model.getDomainModel(),
+                LearningSituation.collectDecisionTreeVariables(model.getDomainModel()),
+                null
+        );
+
+        DecisionTreeTrace decisionTreeTrace= DecisionTreeReasoner.solve(
+                model.getDecisionTree(),
+                situation
+        );
+        System.out.println(decisionTreeTrace.toString());
     }
 }
