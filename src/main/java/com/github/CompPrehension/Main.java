2c625c6c38e70477cf05426977325bb9b80f87b2
Silver <zatmonhp@yandex.ru>
2026-04-25 13:37:25 +0300

    Hello-world: передача параметров в дерево решений

--- src/main/java/com/github/CompPrehension/Main.java
+++ src/main/java/com/github/CompPrehension/Main.java
@@ -1,12 +1,17 @@
 package com.github.CompPrehension;
 
 import its.model.DomainSolvingModel;
+import its.model.definition.DomainModel;
+import its.model.definition.ObjectContainer;
+import its.model.definition.ObjectRef;
 import its.reasoner.LearningSituation;
 import its.reasoner.nodes.DecisionTreeReasoner;
 import its.reasoner.nodes.DecisionTreeTrace;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,14 +24,24 @@ public class Main {
     
     private static void helloWorld() throws FileNotFoundException {
         DomainSolvingModel model = new DomainSolvingModel("model", DomainSolvingModel.BuildMethod.LOQI);
+        DomainModel domain = model.getDomainModel();
+        Map<String, ObjectRef> variables = LearningSituation.collectDecisionTreeVariables(model.getDomainModel());
+        ObjectContainer objects = domain.getObjects();
+
+
+        ObjectRef owner = objects.get("Alice").getReference();
+        ObjectRef pet = objects.get("Basya").getReference();
+
+        variables.put("owner", owner);
+        variables.put("pet", pet);
 
         LearningSituation situation = new LearningSituation(
                 model.getDomainModel(),
-                LearningSituation.collectDecisionTreeVariables(model.getDomainModel()),
+                variables,
                 null
         );
 
-        DecisionTreeTrace decisionTreeTrace= DecisionTreeReasoner.solve(
+        DecisionTreeTrace decisionTreeTrace = DecisionTreeReasoner.solve(
                 model.getDecisionTree(),
                 situation
         );
